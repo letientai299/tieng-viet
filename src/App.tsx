0199695f41aba70570c4141424b3701545e71d3b
Tai <letientai299@gmail.com>
2025-01-01 18:35:18 +0700

    Add github workflows

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,5 @@
-import { useEffect, useRef, useState } from 'react';
-import Word from './Word.tsx';
-import { Poem } from './Poem.ts';
-import Words from './util/words.ts';
+import { useState } from 'react';
 import Pronounce from './Pronounce.tsx';
-
-function Accents() {
-  const space = `\u2800`;
-  const wrap = (s: string) => `${space}${s}`;
-
-  return (
-    <div
-      style={{
-        display: 'flex',
-        flexDirection: 'row',
-        minWidth: '100%',
-        margin: 'auto',
-        gap: '0.5rem',
-      }}
-    >
-      <Word word={wrap(Words.accentMark('grave'))} />
-      <Word word={wrap(Words.accentMark('acute'))} />
-      <Word word={wrap(Words.accentMark('hook'))} />
-      <Word word={wrap(Words.accentMark('tilde'))} />
-      <Word word={wrap(Words.accentMark('dot'))} />
-    </div>
-  );
-}
 
 function App() {
   const [word, setWord] = useState('mẹ');
@@ -51,35 +25,6 @@ function App() {
       <Pronounce word={'nhà'} />
       <Pronounce word={'ngã'} />
     </>
-  );
-}
-
-function Input() {
-  const [text, setText] = useState(Poem);
-  const audioElem = useRef<HTMLAudioElement | null>(null);
-  useEffect(() => {
-    audioElem.current?.load();
-  }, [text]);
-
-  const audioUrl = Words.audioUrl(text);
-
-  return (
-    <div className="card" style={{ display: 'flex', flexDirection: 'column' }}>
-      <h2>Input</h2>
-      <textarea
-        style={{ minHeight: '4rem' }}
-        id="word"
-        placeholder={'Input word to speak'}
-        value={text}
-        onChange={(e) => {
-          setText(e.target.value);
-        }}
-      />
-      <audio controls={true} ref={audioElem}>
-        <source src={audioUrl} type="audio/mpeg" />
-        Your browser does not support the audio element.
-      </audio>
-    </div>
   );
 }
 
